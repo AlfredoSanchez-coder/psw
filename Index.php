354c63f3d6faf5cef3025f3de5e3336b42ac1d16
Alfredo <alfredobillis@gmail.com>
2020-02-26 23:09:02 -0600

    first

---
+++ Index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>MI CURRICULUM</title>
+</head>
+<link rel="stylesheet" type =text/css href="css/Estilos.css">
+<body background="img/FONDO.jpeg">
+
+	
+
+		<div id="animacion">
+			<nav><marquee direction="right" behavior="alternate"><img src="img/TEXTO.png"></marquee></nav>
+		</div>	
+		
+		<div id="principal">
+			<img id="hola" src="img/FOTOALFREDO.jpg" width="120" style="float:left">
+
+			<article align="center">
+
+			
+			<h1 align="center">Datos personales</h1>
+			<p align="center"><b>Nombre: </b>S&aacutenchez Mart&iacutenez Alfredo</p>
+			<p align="center"><b>DIRECCION: </b> C Francisco I Madero #1216, la Cañada, San Rafael, Tlalmanalco, Estado de M&eacutexico</p>
+			<p align="center"><b>TEL: </b>(55) 8598 9488 </p>
+			<p align="center"><b>E-MAIL: </b> alfredo.sanchez.mtz2003@.hotmail.com</p>
+			<p align="center"><b>Fecha de nacimiento: </b> 8 de Julio de 2003</p>
+			<p align="center"><b>Lugar de nacimiento: </b>Chalco, Estado de M&eacutexico</p>
+			</article>
+			<br>
+			<br>
+
+			<section>
+				<fieldset >
+					<legend><b><font color="black">FORMACION ACADEMICA</font></b></legend>
+
+					<table align="center">
+						<tr><td><font><li><b> Estudios:</b></li></font></td></tr>
+						<tr><td><font><b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Educacion preescolar: </b> Colegio Valle de las Rosas</font></td></tr>
+						<tr><td><font><b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Educaci&oacuten primaria: </b> Escuela Lic. Benito Ju&aacuterez </font></td></tr>
+						<tr><td><font><b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Educaci&oacuten secundaria: </b> Escuela J. Encarnaci&oacuten F. C&oacuterdoba </font></td></tr>
+						<tr><td><font><b>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Nivel Medio Superior: </b> Centro de Estudios Cient&iacuteficos y Tecnol&oacutegicos No. 9 "Juan de Dios B&aacutetiz"</font></td></tr>
+					</table>
+				</fieldset>
+			</section>
+			<br>
+			<br>
+			<section>
+				<fieldset>
+					<legend><b><font>EXPERIENCIA</font></b></legend>
+					<table align="center">
+						<tr><td><font><b> Manejo de programas:</b></font></td></tr>
+						<tr><td><font><li>Microsoft Word </li></font></td></tr>
+						<tr><td><font><li>Microsoft Power Point</li></font></td></tr>
+						<tr><td><font><li> Microsoft Exel</li></font></td></tr>
+						<tr><td><font><li> Microsoft Publisher</li></font></td></tr>
+						<tr><td><font><li> Netbeans </li></font></td></tr>
+						<tr><td><font><li> Sublime Text</li></font></td></tr>
+						<tr><td><font><li> Anaconda</li></font></td></tr>
+						<tr><td><font><b> Conocimiento en:</b></font></td></tr>
+						<tr><td><font><li> Lenguajes de programacion java y python</li></font></td>
+						<tr><td><font><li> Manejo de HTML5 y CSS3 </li></font></td></tr></tr>
+						<tr><td><font><li> Matem&aacuteticas</li></font></td></tr>
+						<tr><td><font><li> Expresi&oacuten Oral y Escrita</li></font></td></tr>
+						<tr><td><font><li> Investigaci&oacuten</li></font></td></tr>
+					</table>
+
+				</fieldset>
+			</section>
+
+			<br>
+			<br>
+			<fieldset>
+				<table align="center">
+				<legend><b><font>APTITUDES</font></b></legend>
+				<tr><td><font><li> Comprender el ingl&eacutes de forma escrita.</li></font></td></tr>
+				<tr><td><font><li> Capacidad de comprender lenguajes de programacion.</li></font></td></tr>
+				<tr><td><font><li> Responsabilidad en el trabajo.</li></font></td></tr>
+				<tr><td><font><li> Planificar y desarrollar proyectos relacionados con el software.</li></font></td></tr>
+				<tr><td><font><li> Investigaci&oacuten</li></font></td></tr>
+				</table>
+			</fieldset>
+			<br>
+			<br>
+
+			<aside>
+				<table align="center">
+					
+				<TR>
+
+				<TD><img src="img/SECUN.jpg" width="240" height="180"></td>
+
+				<TD><img src="img/OFF.jpg" width="360" height="180">"></td>
+
+				<TD><img src="img/MAT.jpg" width="360" height="180">"></td>
+
+				</tr>
+
+
+				</table>
+			</aside>
+
+		</div>
+
+
+	<footer align="center">
+		<hr>
+		<br>
+		<br>
+		<p>Este es mi curriculum vitae, para m&aacutes informaci&oacuten  solicitar al correo alfredo.sanchez.mtz.2003@hotmail.com</p>
+		<br>
+		<br>
+		<hr>
+	</footer>
+
+</body>
+</html>
